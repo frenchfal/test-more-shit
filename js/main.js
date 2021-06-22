14d5dd0d372a3c93d075e1371dcc3821586117d5
Baptiste FALLIGAN <bapfal92@gmail.com>
2021-06-22 15:57:53 +0200

    remove console log + add more videos

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ if (document.getElementById('index')) {
         var rect = e.target.getBoundingClientRect();
         var x = e.clientX - rect.left; //x position within the element.
         var y = e.clientY - rect.top;  //y position within the element.
-        console.log("Left? : " + x + " ; Top? : " + y + ".");
 
         if (x > 330 && x < 340 && y > 165 && y < 170) {
             window.location.href = "bienjoue.html"
